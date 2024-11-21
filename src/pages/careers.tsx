3b6a524130b35aeae3f503285f02f64230d93ece
Michael Kalish <michael.kalish@focusconsulting.io>
2024-11-21 10:24:29 -0500

    Fix spacing for job board embed

--- src/pages/careers.tsx
+++ src/pages/careers.tsx
@@ -1,10 +1,13 @@
+import { FaSpinner } from 'react-icons/fa'
 import Layout from '../layout/Layout'
 import React from 'react'
 import { Script } from 'gatsby'
 
 const JobBoard = () => (
     <Layout>
-        <div id="ashby_embed" style={{ minHeight: '50vh' }} />
+        <div id="ashby_embed" style={{ minHeight: '55vh' }}>
+            <FaSpinner />
+        </div>
         <Script src="https://jobs.ashbyhq.com/focus/embed"></Script>
     </Layout>
 )
